9fc54a28b85f3f9c613d91198e8d8411707a9e96
Adesh <adeshkrmaharaj16@gmail.com>
2024-11-05 12:28:50 +0530

    minor ts fixes

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Star, ShoppingCart, ArrowLeft } from 'lucide-react';
 import { products } from '../data/products';
